ff5f1fabba09907acd38fa5cca45513f8ff36dbb
keombre <keombre8@gmail.com>
2021-03-02 02:12:45 +0100

    fix stop id

--- src/scripts/train_stops.lua
+++ src/scripts/train_stops.lua
@@ -23,7 +23,7 @@ function train_stops.get_all_cleanup(network, carriages)
     for _, stop in pairs(game.get_train_stops()) do
         if stop.valid and train_stops.is_cleanup(stop.backer_name) then
             if network == nil or not ltn.is_ltn_stop(stop.unit_number) or
-                (ltn.get_network(stop.unit_number) == network and ltn.is_carriage_in_limit(carriages)) then
+                (ltn.get_network(stop.unit_number) == network and ltn.is_carriage_in_limit(stop.unit_number, carriages)) then
 
                 local processes = {
                     generic_item = false,
