16be9f9ce1c9578c9d488be947007a883ea32307
keombre <keombre8@gmail.com>
2021-03-15 00:20:26 +0100

    error handling

--- src/scripts/format.lua
+++ src/scripts/format.lua
@@ -1,14 +1,8 @@
 local format = {}
--- local status, serpent = pcall(require, "__debug/serpent")
--- 
--- if status then
---     function format.debug(msg)
---         game.print(serpent.line(msg))
---     end
--- else
---     function format.debug(msg)
---     end
--- end
+
+function format.debug(msg)
+    game.print(serpent.line(msg))
+end
 
 function format.all(msg)
     game.print("[color=yellow][LTN Cleanup][/color] " .. msg)
@@ -46,6 +40,14 @@ end
 
 function format.train_depot_alert(train)
     format.alert("Train " .. format.train(train) .. " will arrive at depot with remaining cargo")
+end
+
+function format.show_fatal(error, msg)
+    if game.is_multiplayer() then
+        game.print("[color=red][LTN Cleanup Error] Unexpected error occured.[/color] [color=orange]Error: " .. serpent.line(error) .. "[/color]\n" .. msg)
+    else
+        game.show_message_dialog{text="[font=default-large-bold]LTN Cleanup[/font][font=default-bold] - Unexpected error occured[/font]\n\n[color=orange]Error: " .. error .. "[/color]\n\n" .. msg .. "\n To continue press: "}
+    end
 end
 
 return format
